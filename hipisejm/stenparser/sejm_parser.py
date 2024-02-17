d7cc793d3a730c5235bc85e891ca75b3e59603f5
Marcin Walas <walasiek@gmail.com>
2024-02-17 13:16:29 +0100

    refactor pdfminer wrapping to separate class for clean code

--- hipisejm/stenparser/sejm_parser.py
+++ hipisejm/stenparser/sejm_parser.py
@@ -1,12 +1,7 @@
 import re
 import logging
-from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextContainer, LAParams, LTTextBox, LTTextLine, LTChar
-from pdfminer.pdfparser import PDFParser
-from pdfminer.converter import PDFPageAggregator
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+from hipisejm.utils.pdfminer_wrapper import PDFMinerWrapper
 
 
 class SejmParser:
@@ -33,59 +28,9 @@ class SejmParser:
     def _parse_pdf_to_raw(self, filepath: str):
         raw_results = []
         with open(filepath, "rb") as pdffile:
-            self.pdf_parser = PDFParser(pdffile)
-            document = PDFDocument(self.pdf_parser)
-            resource_manager = PDFResourceManager()
             laparams = LAParams(
                 char_margin=self.PDF_CHAR_MARGIN,
                 line_margin=self.PDF_LINE_MARGIN)
-
-            dev = PDFPageAggregator(resource_manager, laparams=laparams)
-            intepreter = PDFPageInterpreter(resource_manager, dev)
-
-            for pdf_page in PDFPage.create_pages(document):
-                self.number_of_pages += 1
-
-                # debug
-                #if self.number_of_pages > 10:
-                #    break
-                logging.debug("Parsing PDF page %i", self.number_of_pages)
-
-                intepreter.process_page(pdf_page)
-                layout = dev.get_result()
-
-                for element in layout:
-                    if isinstance(element, LTTextContainer):
-                        self._parse_text_container(raw_results, element)
-
-                print("########## END PAGE ##########")
-
-
-    def _parse_text_container(self, raw_results, text_container):
-        for text_line in text_container:
-
-            current_fontname = None
-            chunk = []
-
-            for character in text_line:
-                if not isinstance(character, LTChar):
-                    continue
-
-                if (current_fontname is not None) and (current_fontname != character.fontname):
-                    entry = ("".join(chunk), current_fontname, text_line.height)
-                    raw_results.append(entry)
-                    print(entry)
-                    current_fontname = character.fontname
-                    chunk = [character.get_text()]
-                else:
-                    current_fontname = character.fontname
-                    chunk.append(character.get_text())
-            if len(chunk) > 0:
-                entry = ("".join(chunk), current_fontname, text_line.height)
-                raw_results.append(entry)
-                print(entry)
-
-            print("------ END LINE--------")
-        print("==========END BOX==========")
-
-        return raw_results
+            pdfminer_wrapper = PDFMinerWrapper(pdffile, laparams=laparams)
+            pdfminer_wrapper.parse()
+            self.number_of_pages = pdfminer_wrapper.number_of_pages
---
+++ hipisejm/utils/pdfminer_wrapper.py
@@ -0,0 +1,78 @@
+"""
+Wraps some PDFMiner functions to simplify text extraction
+"""
+import logging
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer, LAParams, LTTextBox, LTTextLine, LTChar
+from pdfminer.pdfparser import PDFParser
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from typing import BinaryIO
+
+
+class PDFMinerWrapper:
+    def __init__(self, file_to_parse: BinaryIO, laparams: LAParams = None):
+        """
+        params:
+        filepath - file to be parsed
+        laparams - if None, then uses PDFMiner default LAParams, otherwise uses LAParams provided here
+        """
+        self.parsed_data = []
+        self.number_of_pages = 0
+
+        self.pdf_parser = PDFParser(file_to_parse)
+        self.resource_manager = PDFResourceManager()
+
+        self.dev = PDFPageAggregator(self.resource_manager, laparams=laparams)
+        self.intepreter = PDFPageInterpreter(self.resource_manager, self.dev)
+
+    def parse(self):
+        self.parsed_data = []
+        self.number_of_pages = 0
+
+        document = PDFDocument(self.pdf_parser)
+        for pdf_page in PDFPage.create_pages(document):
+            self.number_of_pages += 1
+
+            # debug
+            #if self.number_of_pages > 10:
+            #    break
+            logging.debug("Parsing PDF page %i", self.number_of_pages)
+
+            self.intepreter.process_page(pdf_page)
+            layout = self.dev.get_result()
+
+            for element in layout:
+                if isinstance(element, LTTextContainer):
+                    self._parse_text_container(element)
+
+            print("########## END PAGE ##########")
+
+    def _parse_text_container(self, text_container):
+        for text_line in text_container:
+
+            current_fontname = None
+            chunk = []
+
+            for character in text_line:
+                if not isinstance(character, LTChar):
+                    continue
+
+                if (current_fontname is not None) and (current_fontname != character.fontname):
+                    entry = ("".join(chunk), current_fontname, text_line.height)
+                    self.parsed_data.append(entry)
+                    print(entry)
+                    current_fontname = character.fontname
+                    chunk = [character.get_text()]
+                else:
+                    current_fontname = character.fontname
+                    chunk.append(character.get_text())
+            if len(chunk) > 0:
+                entry = ("".join(chunk), current_fontname, text_line.height)
+                self.parsed_data.append(entry)
+                print(entry)
+
+            print("------ END LINE--------")
+        print("==========END BOX==========")
