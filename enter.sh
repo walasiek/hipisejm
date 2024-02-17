f344cc8e69491e0e6f2d84bf5027902b2a576959
Marcin Walas <walasiek@gmail.com>
2024-02-17 10:54:36 +0100

    fix enter.sh

--- enter.sh
+++ enter.sh
@@ -17,17 +17,6 @@ if [[ ! -d $VIRTUALENV_DIR ]]; then
     python -m ipykernel install --user --name=$ENV_DIRNAME
     echo "Please set up your password to jupyter for easier usage:"
     jupyter notebook password
-    # See: https://jupyter-dashboards-layout.readthedocs.io/en/latest/getting-started.html
-    echo "Configure jupyter dashboard..."
-    jupyter dashboards quick-setup --sys-prefix
-    # I dont know is it needed?
-    jupyter nbextension enable jupyter_dashboards --py --sys-prefix
-
-    # Spacy models
-
-    # used for Lemmatization
-    python -m spacy download pl_core_news_sm
-    python -m spacy download pl_core_news_md
 else
     export PYTHONPATH=$PYTHONPATH:`pwd`
     source $VIRTUALENV_DIR/bin/activate
